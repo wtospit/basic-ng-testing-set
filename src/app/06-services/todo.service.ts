c5f5070b28186abb8ed3ae087306bad072d77d21
Witaya Tospitakkul <witaya1412@gmail.com>
2019-01-10 22:43:12 +0700

    init boilerplate

---
+++ src/app/06-services/todo.service.ts
@@ -0,0 +1,21 @@
+
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs';
+
+export class TodoService { 
+  constructor(private http: HttpClient) { 
+  }
+
+  add(todo) {
+    return this.http.post('...', todo);
+  }
+
+  getTodos(): Observable<any[]> { 
+    return this.http.get<any[]>('...');
+  }
+
+  delete(id) {
+    return this.http.delete('...');
+  }
+}
